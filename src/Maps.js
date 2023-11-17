0d6bedd84b76e63c929f899da05ee2e05b10dfca
Strox7 <113477645+Strox7@users.noreply.github.com>
2023-11-17 15:38:28 +0100

    Update Maps.js

--- src/Maps.js
+++ src/Maps.js
@@ -8,9 +8,10 @@ function Maps() {
         width="100%"
         height="500"
         style={{ border: "0" }}
-        allowfullscreen=""
+        allowFullScreen=""
         loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="google map"
       ></iframe>
     </div>
   );
